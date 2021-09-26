a2e118e8b2f914c01fd789ab1c4e5697b7f4b1bb
pawas <pawas.saxena@gmail.com>
2021-09-26 16:54:36 +0530

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Counter/>
+     <Button variant="primary">Button #1</Button>
     </div>
   );
 }
