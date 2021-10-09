5e9acb78d2d53ec5f643683a858ee1acd7ba3668
pawas <pawas.saxena@gmail.com>
2021-10-10 00:39:57 +0530

    fixed code for checkout page

--- src/features/checkout/Checkout.tsx
+++ src/features/checkout/Checkout.tsx
@@ -4,6 +4,9 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import styles from './checkout.module.css';
+import { useSelector } from 'react-redux';
+import { selectCart } from '../cart/ cartSlice';
+import { Product } from '../../shared/types';
 
 type CheckoutFormDetails = {
   email: string;
@@ -12,7 +15,39 @@ type CheckoutFormDetails = {
   phone: string;
 };
 
+type AllowedCoupons = 'DIWALI20' | 'NEW30' | 'NONE';
+const couponDiscountFactor: Record<AllowedCoupons, number> = {
+  DIWALI20: 0.8,
+  NEW30: 0.7,
+  NONE: 1,
+};
+
+const SingleProductForCheckout: React.FC<{ product: Product }> = ({ product }) => {
+  return (
+    <div className={styles['single-product-root']}>
+      <Row>
+        <Col>
+          <img src={(product.id % 6) + '.jpg'} className={styles.image}></img>
+        </Col>
+        <Col>
+          <div>
+            {product.car_model} : {product.car}
+            <div className={styles.description}>
+              <div>Color: {product.car_color}</div>
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <div>${product.price}</div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
 const Checkout: React.FC<{}> = ({}) => {
+  const { cartProducts } = useSelector(selectCart);
+
   const [userDetails, setUserDetails] = useState<CheckoutFormDetails>({
     email: '',
     name: '',
@@ -20,7 +55,14 @@ const Checkout: React.FC<{}> = ({}) => {
     phone: '',
   });
 
-  const [coupon, setCoupon] = useState<string>('');
+  const [coupon, setCoupon] = useState<AllowedCoupons>('NONE');
+
+  const getProductTotal = () => {
+    const totalProductPrice = cartProducts.reduce((prev: number, curr: Product) => {
+      return prev + parseInt(curr.price);
+    }, 0);
+    return Math.ceil(totalProductPrice * couponDiscountFactor[coupon]);
+  };
 
   return (
     <div className={styles['checkout-root']}>
@@ -79,11 +121,12 @@ const Checkout: React.FC<{}> = ({}) => {
             </Button>
           </Form>
         </Col>
-        <Col>
-          <h1>Total Panel</h1>
-          {JSON.stringify(userDetails)}
-          {coupon}
 
+        <Col>
+          {cartProducts.map((product: Product) => {
+            return <SingleProductForCheckout product={product} />;
+          })}
+          <br />
           <Form.Group className="mb-2" controlId="Phone">
             <Form.Label>Coupon</Form.Label>
             <Form.Control
@@ -91,14 +134,16 @@ const Checkout: React.FC<{}> = ({}) => {
               placeholder="Phone"
               value={coupon}
               onChange={(e) => {
-                setCoupon(e.target.value);
+                setCoupon(e.target.value as AllowedCoupons);
               }}
             >
-              <option>Select Coupon</option>
+              <option value="NONE">Select Coupon</option>
               <option value="DIWALI20">DIWALI20</option>
               <option value="NEW30">NEW30</option>
             </Form.Control>
           </Form.Group>
+
+          <div>Total: $ {getProductTotal()}</div>
         </Col>
       </Row>
     </div>
