efef3cbee802c1646425d882feb8a5cdbcf345ef
pawas <pawas.saxena@gmail.com>
2021-10-09 23:11:18 +0530

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import ProductList from './features/productList/ProductList';
 import './App.css';
 import Filter from './features/filter/Filter';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Cart from './features/cart/Cart';
 
 function App() {
   return (
     <div className="App">
-      <Filter />
-      <ProductList />
+      <Router>
+        <Navbar bg="light" expand="lg" fixed="top">
+          <Nav className="me-auto">
+            <Nav.Link href="/">
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link href="/">
+              <Link to="/checkout">Cart</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+
+        <Switch>
+          <Route path="/checkout">
+            <Cart />
+          </Route>
+          <Route path="/">
+            <>
+              <Filter />
+              <ProductList />
+            </>
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );  
+  );
 }
 
 export default App;
